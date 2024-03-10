91837bee262ae32aa16900c6aace1bd93e9f039e
Ivan Zabaluev <ivaiika@yandex.ru>
2024-03-10 13:25:10 +0300

    qwe

--- lua/solarized-osaka/theme.lua
+++ lua/solarized-osaka/theme.lua
@@ -294,9 +294,9 @@ function M.setup()
     ["@property"] = { link = "Identifier" },
 
     --- Identifiers
-    ["@variable"] = { fg = c.base0, style = options.styles.variables }, -- Any variable name that does not have another highlight.
-    ["@variable.builtin"] = { fg = c.orange500 }, -- Variable names that are defined by the languages, like `this` or `self`.
-    ["@module.builtin"] = { fg = c.orange500 }, -- Variable names that are defined by the languages, like `this` or `self`.
+    ["@variable"] = { fg = "#EEFFFF", style = options.styles.variables }, -- Any variable name that does not have another highlight.
+    ["@variable.builtin"] = { fg = "#FF5370" }, -- Variable names that are defined by the languages, like `this` or `self`.
+    ["@module.builtin"] = { fg = "#FF5370" }, -- Variable names that are defined by the languages, like `this` or `self`.
 
     --- Text
     -- ["@markup.raw.markdown"] = { fg = c.blue },
